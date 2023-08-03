f60c6f14f13c2c0d94b02c043b5beda88b5d3e1f
Ashley Rivera <ashleyariv@gmail.com>
2023-08-03 15:39:18 -0400

    functions lab continued

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+function saturdayFun (activity) {
+    if (activity === "bathe my dog") {
+    return `This Saturday, I want to ${activity}!`
+    }else {
+        return "This Saturday, I want to roller-skate!"
+    }
+}
+
+function mondayWork(work) {
+    if (work === "work from home") {
+        return `This Monday, I will ${work}.`
+    }else {
+        return "This Monday, I will go to the office."
+        }
+}
+
+function wrapAdjective(msg = "*") {
+    return (thing = "special") => {
+        return `You are ${msg}${thing}${msg}!`
+    }
+}
